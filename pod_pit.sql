49bfb672b24ea8c48cc9bd9b7b105f2ada00a032
jthomm <jthomm@yahoo.com>
2016-02-19 17:18:03 -0500

    DDL for Pod projections

---
+++ pod_pit.sql
@@ -0,0 +1,37 @@
+CREATE TABLE pod_pit (
+    name TEXT
+  , fg_id TEXT
+  , lg TEXT
+  , team TEXT
+  , role TEXT
+  , gs FLOAT
+  , ip FLOAT
+  , ip_s FLOAT
+  , tbf FLOAT
+  , w FLOAT
+  , l FLOAT
+  , qs FLOAT
+  , sv FLOAT
+  , era FLOAT
+  , h FLOAT
+  , r FLOAT
+  , er FLOAT
+  , hr FLOAT
+  , so FLOAT
+  , bb FLOAT
+  , hbp FLOAT
+  , k_9 FLOAT
+  , bb_9 FLOAT
+  , hr_9 FLOAT
+  , k_pct FLOAT
+  , bb_pct FLOAT
+  , baa FLOAT
+  , whip FLOAT
+  , babip FLOAT
+  , lob FLOAT
+  , gb_pct FLOAT
+  , ld_pct FLOAT
+  , fb_pct FLOAT
+  , hr_fb FLOAT
+)
+;
