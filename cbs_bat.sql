551b1c4a469214856d473342b53077c5f0ffb8ca
jthomm <jthomm@yahoo.com>
2016-02-19 17:18:23 -0500

    DDL for CBS projections

---
+++ cbs_bat.sql
@@ -0,0 +1,22 @@
+CREATE TABLE cbs_bat (
+    pos TEXT
+  , cbs_id TEXT
+  , name TEXT
+  , ab INT
+  , r INT
+  , h INT
+  , b1 INT
+  , b2 INT
+  , b3 INT
+  , hr INT
+  , rbi INT
+  , bb INT
+  , so INT
+  , sb INT
+  , cs INT
+  , ba FLOAT
+  , obp FLOAT
+  , slg FLOAT
+  , fpts INT
+)
+;
---
+++ cbs_pit.sql
@@ -0,0 +1,20 @@
+CREATE TABLE cbs_pit (
+    pos TEXT
+  , cbs_id TEXT
+  , name TEXT
+  , ip FLOAT
+  , gs INT
+  , qg INT
+  , cg INT
+  , w INT
+  , l INT
+  , sv INT
+  , bs INT
+  , k INT
+  , bb INT
+  , h INT
+  , era FLOAT
+  , whip FLOAT
+  , fpts INT
+)
+;
