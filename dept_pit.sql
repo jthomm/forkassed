c96638a41616406ccea24546cba72eb05fa21dc9
jthomm <jthomm@yahoo.com>
2016-03-25 00:17:36 -0400

    FanGraphs depth charts

---
+++ dept_pit.sql
@@ -0,0 +1,24 @@
+CREATE TABLE zips_pit (
+    fg_name TEXT
+  , team TEXT
+  , w INT
+  , l INT
+  , sv INT
+  , hld INT
+  , era FLOAT
+  , gs INT
+  , g INT
+  , ip FLOAT
+  , h INT
+  , er INT
+  , hr INT
+  , so INT
+  , bb INT
+  , whip FLOAT
+  , k_9 FLOAT
+  , bb_9 FLOAT
+  , fip FLOAT
+  , war FLOAT
+  , fg_id TEXT
+)
+;
