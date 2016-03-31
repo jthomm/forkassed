dddf7e93872bce8934c24ecfde47a76df133dfcc
jthomm <jthomm@yahoo.com>
2016-03-31 16:35:37 -0400

    Davenport adjusted pitcher projections

---
+++ clay_adj_pit.sql
@@ -0,0 +1,43 @@
+CREATE TABLE clay_adj_pit (
+    last TEXT
+  , first TEXT
+  , team TEXT
+  , lg TEXT
+  , age INT
+  , role TEXT
+  , w
+  , l
+  , sv
+  , g
+  , gs
+  , ip FLOAT
+  , h
+  , r
+  , er
+  , hr
+  , bb
+  , so
+  , gbo
+  , babip FLOAT
+  , trend
+  , whip FLOAT
+  , ra FLOAT
+  , era FLOAT
+  , pera FLOAT
+  , eqera FLOAT
+  , eqh9 FLOAT
+  , eqbb9 FLOAT
+  , eqso9 FLOAT
+  , eqhr9 FLOAT
+  , vorp INT
+  , raa INT
+  , warp FLOAT
+  , upside INT
+  , break INT
+  , improve INT
+  , collapse INT
+  , attrition INT
+  , comparables TEXT
+  , howe_id TEXT
+  , qs INT
+);
