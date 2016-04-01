d0ec197c00c635d78f5bb661a9be74470998fe1e
jthomm <jthomm@yahoo.com>
2016-03-31 23:05:52 -0400

    Projection weights for different stats

---
+++ weights_bat.sql
@@ -0,0 +1,16 @@
+CREATE TABLE weights_bat (
+    source text
+  , g FLOAT
+  , pa FLOAT
+  , ab FLOAT
+  , b1 FLOAT
+  , b2 FLOAT
+  , b3 FLOAT
+  , hr FLOAT
+  , r FLOAT
+  , rbi FLOAT
+  , bb FLOAT
+  , so FLOAT
+  , sb FLOAT
+  , cs FLOAT
+);
