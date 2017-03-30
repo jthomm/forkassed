6c404236dfcdff732c7a2ac03a7bcacb765bc689
jthomm <jthomm@yahoo.com>
2017-03-30 18:00:35 -0400

    Create atc_bat.sql

---
+++ atc_bat.sql
@@ -0,0 +1,25 @@
+CREATE TABLE atc_bat (
+    fg_name TEXT
+  , team TEXT
+  , g INT
+  , pa INT
+  , ab INT
+  , h INT
+  , b2 INT
+  , b3 INT
+  , hr INT
+  , r INT
+  , rbi INT
+  , bb INT
+  , so INT
+  , sb INT
+  , cs INT
+  , avg FLOAT
+  , obp FLOAT
+  , slg FLOAT
+  , ops FLOAT
+  , woba FLOAT
+  , adp FLOAT
+  , fg_id TEXT
+)
+;
