f1d154e4abdc426617e307c738ef533e4d8bb95c
jthomm <jthomm@yahoo.com>
2017-03-30 18:00:06 -0400

    Create atc_pit.sql

---
+++ atc_pit.sql
@@ -0,0 +1,23 @@
+CREATE TABLE atc_pit (
+    fg_name TEXT
+  , team TEXT
+  , w INT
+  , l INT
+  , sv INT
+  , era FLOAT
+  , gs INT
+  , g INT
+  , ip FLOAT
+  , h INT
+  , er INT
+  , hr INT
+  , so INT
+  , bb INT
+  , whip FLOAT
+  , k_9 FLOAT
+  , bb_9 FLOAT
+  , fip FLOAT
+  , adp FLOAT
+  , fg_id TEXT
+)
+;
